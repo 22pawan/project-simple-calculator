9665f958124d3a08104f3471a64ba8f7d6ceadf0
22pawan <01fe22bca022@kletech.ac.in>
2024-01-20 12:35:30 +0530

    program for arithematic operations

---
+++ calculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        float num1 = scanner.nextFloat();
+        float num2 = scanner.nextFloat();
+        System.out.println("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+        float result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error! Dividing by zero is not allowed.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error! Invalid operator.");
+                return;
+        }
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+    }
+}
