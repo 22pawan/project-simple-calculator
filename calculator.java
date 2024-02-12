97cb9f1e012b40eef0e19edc43c79d6502237eb3
01fe22bca022 <157193242+22pawan@users.noreply.github.com>
2024-02-12 16:34:33 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -6,26 +6,26 @@ public class calculator {
         float num1 = scanner.nextFloat();
         float num2 = scanner.nextFloat();
         System.out.println("Enter an operator (+, -, *, /): ");
-        char operator = scanner.next().charAt(0);
+        // char operator = scanner.next().charAt(0);
         float result;
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Error! Dividing by zero is not allowed.");
-                    return;
-                }
-                break;
+// switch (operator) {
+//             case '+':
+//                         result = num1 + num2;
+//                 break;
+//             case '-':
+//                 result = num1 - num2;
+//                 break;
+//             case '*':
+//                 result = num1 * num2;
+//                 break;
+//             case '/':
+//                 if (num2 != 0) {
+//                     result = num1 / num2;
+//                 } else {
+//                     System.out.println("Error! Dividing by zero is not allowed.");
+//                     return;
+//                 }
+//                 break;
             default:
                 System.out.println("Error! Invalid operator.");
                 return;
